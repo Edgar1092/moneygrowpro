522e5606faca1386d34a595912593435649102b1
Menphis <menphisj@gmail.com>
2020-08-29 19:34:58 -0400

    cambios

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://intensityfitnessonline.com/admin/moneygrowpro/backend/api',
+  apiUrl: 'https://intensityfitnessonline.com/test/backend/api',
   serviceWorker: false
 };
